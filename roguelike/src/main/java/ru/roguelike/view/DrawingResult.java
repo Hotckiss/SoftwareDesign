e77d5e56f8261d746fac3cc7b6e42080c775f480
Andrew <kirilenko.haa@gmail.com>
2019-06-02 00:15:07 +0300

    finally fix comments

--- roguelike/src/main/java/ru/roguelike/view/DrawingResult.java
+++ roguelike/src/main/java/ru/roguelike/view/DrawingResult.java
@@ -1,18 +1,34 @@
 package ru.roguelike.view;
 
+/**
+ * Indicates game to load map from file or generate
+ */
 public class DrawingResult {
     private boolean loadMapFromFile;
     private String fileNameForMap;
 
+    /**
+     * Constructs new drawing result
+     * @param loadMapFromFile indicates to load map from file
+     * @param fileNameForMap file with map
+     */
     public DrawingResult(boolean loadMapFromFile, String fileNameForMap) {
         this.loadMapFromFile = loadMapFromFile;
         this.fileNameForMap = fileNameForMap;
     }
 
+    /**
+     * Getter for loading map from file flag
+     * @return true if should load map from file false otherwise
+     */
     public boolean isLoadMapFromFile() {
         return loadMapFromFile;
     }
 
+    /**
+     * Getter for loading map from file filename
+     * @return filename of map
+     */
     public String getFileNameForMap() {
         return fileNameForMap;
     }
