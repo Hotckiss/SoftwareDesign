567dec4a902783219e1f4b55244ad5eb84e749d5
Andrew <kirilenko.haa@gmail.com>
2019-06-03 22:12:40 +0300

    add setup

--- roguelike/src/main/java/ru/roguelike/net/server/RoguelikeServer.java
+++ roguelike/src/main/java/ru/roguelike/net/server/RoguelikeServer.java
@@ -1,11 +1,59 @@
 package ru.roguelike.net.server;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
+import org.jetbrains.annotations.NotNull;
 import ru.roguelike.ConnectionSetUpperGrpc;
 import ru.roguelike.PlayerRequest;
+import ru.roguelike.RoguelikeLogger;
 import ru.roguelike.ServerReply;
 
+import java.io.IOException;
+
 public class RoguelikeServer {
+    private int port;
+    private Server server = null;
+
+    public RoguelikeServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        int port = 22228;
+        RoguelikeServer server = new RoguelikeServer(port);
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private void start() throws IOException {
+        server = ServerBuilder.forPort(port)
+                .addService(new RoguelikeService())
+                .build()
+                .start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                stop();
+                System.err.println("*** server shut down");
+            }
+        }));
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
     private static class RoguelikeService extends ConnectionSetUpperGrpc.ConnectionSetUpperImplBase {
 
         @Override
