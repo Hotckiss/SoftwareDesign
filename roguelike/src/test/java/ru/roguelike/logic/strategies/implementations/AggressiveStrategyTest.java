49eef2d9359ae805d7b516e7c3874190a0f83862
Andrew <kirilenko.haa@gmail.com>
2019-06-09 13:38:38 +0300

    test strategies

---
+++ roguelike/src/test/java/ru/roguelike/logic/strategies/implementations/AggressiveStrategyTest.java
@@ -0,0 +1,42 @@
+package ru.roguelike.logic.strategies.implementations;
+
+import org.junit.Test;
+import ru.roguelike.logic.GameModel;
+import ru.roguelike.logic.GameModelImpl;
+import ru.roguelike.logic.generators.GenerationUtils;
+import ru.roguelike.models.Position;
+import ru.roguelike.models.objects.artifacts.FinalKey;
+import ru.roguelike.models.objects.base.AbstractGameObject;
+import ru.roguelike.models.objects.base.AbstractGameParticipant;
+import ru.roguelike.models.objects.movable.Player;
+import ru.roguelike.view.StringStreamInputProviderImpl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AggressiveStrategyTest {
+
+    @Test
+    public void preferredMove() throws IOException {
+        FinalKey key = new FinalKey(new Position(0, 2));
+        Player player = new Player(new Position(0, 0));
+        AbstractGameParticipant mob = GenerationUtils.makeMob('B', new Position(0 ,1));
+
+        List<List<AbstractGameObject>> field = new ArrayList<>();
+        field.add(new ArrayList<>());
+        field.get(0).add(player);
+        field.get(0).add(mob);
+        field.get(0).add(key);
+
+        GameModel model = new GameModelImpl(field, player, key, Collections.singletonList(mob), new ArrayList<>());
+
+        model.makeMove(new StringStreamInputProviderImpl("a"));
+
+        // player was hit by 40 and regenerate by 5
+        assertEquals(65, player.getHealth());
+    }
+}
---
+++ roguelike/src/test/java/ru/roguelike/logic/strategies/implementations/PassiveStrategyTest.java
@@ -0,0 +1,44 @@
+package ru.roguelike.logic.strategies.implementations;
+
+import org.junit.Test;
+import ru.roguelike.logic.GameModel;
+import ru.roguelike.logic.GameModelImpl;
+import ru.roguelike.logic.generators.GenerationUtils;
+import ru.roguelike.models.Position;
+import ru.roguelike.models.objects.artifacts.FinalKey;
+import ru.roguelike.models.objects.base.AbstractGameObject;
+import ru.roguelike.models.objects.base.AbstractGameParticipant;
+import ru.roguelike.models.objects.map.FreePlace;
+import ru.roguelike.models.objects.movable.Player;
+import ru.roguelike.view.StringStreamInputProviderImpl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PassiveStrategyTest {
+
+    @Test
+    public void preferredMove() throws IOException {
+        FinalKey key = new FinalKey(new Position(0, 3));
+        Player player = new Player(new Position(0, 0));
+        AbstractGameParticipant mob = GenerationUtils.makeMob('V', new Position(0 ,1));
+
+        List<List<AbstractGameObject>> field = new ArrayList<>();
+        field.add(new ArrayList<>());
+        field.get(0).add(player);
+        field.get(0).add(mob);
+        field.get(0).add(new FreePlace(new Position(0, 2)));
+        field.get(0).add(key);
+
+        GameModel model = new GameModelImpl(field, player, key, Collections.singletonList(mob), new ArrayList<>());
+
+        model.makeMove(new StringStreamInputProviderImpl("a"));
+
+        //mob not moved
+        assertEquals(new Position(0, 1), mob.getPosition());
+    }
+}
