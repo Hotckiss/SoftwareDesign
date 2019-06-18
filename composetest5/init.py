a65e72d9c772ba29cceb5ba511dcd20969345e16
Andrew <kirilenko.haa@gmail.com>
2019-06-18 14:34:37 +0300

    Update init.py

--- composetest5/init.py
+++ composetest5/init.py
@@ -3,12 +3,17 @@ import time
 
 time.sleep(20)
 sqs = None
-while True:
+triesCount = 0
+timeoutCount = 30
+while triesCount < timeoutCount:
     try:
         sqs = boto3.resource(service_name='sqs', endpoint_url='http://localstack:4576',
                              aws_access_key_id="key", aws_secret_access_key="password")
         break
     except:
+        triesCount += 1
+        if (triesCount + 1) == timeoutCount:
+            print("Timeout was reached!")
         time.sleep(1)
         continue
     
