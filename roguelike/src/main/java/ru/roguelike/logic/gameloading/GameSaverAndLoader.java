ffe4773ad49a84317f66f3f7dc7931a0fb0e99bd
Andrew <kirilenko.haa@gmail.com>
2019-06-21 00:35:31 +0300

    fix sava/load

--- roguelike/src/main/java/ru/roguelike/logic/gameloading/GameSaverAndLoader.java
+++ roguelike/src/main/java/ru/roguelike/logic/gameloading/GameSaverAndLoader.java
@@ -16,6 +16,7 @@ import ru.roguelike.models.objects.movable.Player;
 import ru.roguelike.view.Drawable;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,52 +49,24 @@ public class GameSaverAndLoader {
     public void saveGame(GameModel game) throws FileNotFoundException {
         clearFile(savedGameFile);
 
-        List<List<AbstractGameObject>> fieldModel = game.getField();
-        Player player = game.getActivePlayer();
-        FinalKey key = game.getKey();
-        List<Mob> mobs = game.getMobs();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out;
+        FileOutputStream fos = new FileOutputStream(savedGameFile);
 
-        // FIELD INFO
-        int n = fieldModel.size();
-        int m = fieldModel.get(0).size();
-        appendToFile(savedGameFile, n + " " + m + "\n");
-
-        for (List<AbstractGameObject> line : fieldModel) {
-            StringBuilder builder = new StringBuilder();
-            for (Drawable drawable : line) {
-                builder.append(drawable.getDrawingFigure());
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(game);
+            out.flush();
+            fos.write(bos.toByteArray());
+            fos.flush();
+        } catch (IOException e) {
+            RoguelikeLogger.INSTANCE.log_error(e.getMessage());
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                RoguelikeLogger.INSTANCE.log_info(ex.getMessage());
             }
-
-            appendToFile(savedGameFile, builder.toString() + "\n");
-        }
-
-        // PLAYER INFO
-        Position position = player.getPosition();
-        appendToFile(savedGameFile, position.getX() + " " + position.getY() + "\n");
-        appendToFile(savedGameFile,  player.getExperience() + "\n");
-        appendToFile(savedGameFile, player.getArtifactsLog(false) + "\n");
-        List<Player.ArtifactInInventory> artifactInInventories = player.getArtifacts();
-        for (int i = 0; i < artifactInInventories.size(); i++) {
-            appendToFile(savedGameFile, artifactInInventories.get(i).equipped() ? "true" : "false");
-            if (i != artifactInInventories.size() - 1) {
-                appendToFile(savedGameFile, " ");
-            }
-        }
-
-        appendToFile(savedGameFile, "\n");
-
-        appendToFile(savedGameFile, getFeaturesAsString(player) + "\n");
-
-        // KEY INFO
-        Position keyPosition = key.getPosition();
-        appendToFile(savedGameFile, keyPosition.getX() + " " + keyPosition.getY() + "\n");
-
-        // MOBS INFO
-        appendToFile(savedGameFile, mobs.size() + "\n");
-        for (AbstractGameParticipant mob : mobs) {
-            Position mobPosition = mob.getPosition();
-            appendToFile(savedGameFile, mobPosition.getX() + " " + mobPosition.getY() + "\n");
-            appendToFile(savedGameFile, getFeaturesAsString(mob) + "\n");
         }
     }
 
@@ -101,139 +74,17 @@ public class GameSaverAndLoader {
      * Method that loads game model from file
      * @return  loaded game
      */
-    public GameModel loadGame() {
+    public GameModel loadGame() throws IOException, ClassNotFoundException {
         File file = new File(savedGameFile);
         if (!file.exists()) {
             return null;
         }
 
-        List<List<AbstractGameObject>> field = new ArrayList<>();
-        Player player = null;
-        FinalKey key = null;
-        List<Mob> mobs = new ArrayList<>();
-        List<Artifact> artifacts = new ArrayList<>();
-
-        List<String> mapFromFile = new ArrayList<>();
-
-        try(BufferedReader reader = new BufferedReader(new FileReader(savedGameFile))) {
-
-            // GETTING FIELD INFO
-            String line = reader.readLine();
-            String[] sizes = line.split("\\s+");
-            int n = Integer.parseInt(sizes[0]);
-            int m = Integer.parseInt(sizes[1]);
-
-            for (int i = 0; i < n; i++) {
-                line = reader.readLine();
-                List<AbstractGameObject> lineList = new ArrayList<>();
-                mapFromFile.add(line);
-
-                for (int j = 0; j < m; j++) {
-                    char c = line.charAt(j);
-                    switch (c) {
-                        case '.':
-                            lineList.add(new FreePlace(new Position(i, j)));
-                            break;
-                        case '#':
-                            lineList.add(new Wall(new Position(i, j)));
-                            break;
-                        case 'c':
-                        case 'g':
-                        case 'f':
-                        case 'h':
-                        case 'r':
-                        case 's':
-                            Artifact artifact = GenerationUtils.makeArtifact(c, new Position(i, j));
-                            artifacts.add(artifact);
-                            lineList.add(artifact);
-                            break;
-                        default:
-                            lineList.add(new FreePlace(new Position(i, j)));
-                            break;
-
-                    }
-                }
-                field.add(lineList);
-            }
-
-            // GETTING PLAYER INFO
-            line = reader.readLine();
-            String[] coords = line.split("\\s+");
-            int x = Integer.parseInt(coords[0]);
-            int y = Integer.parseInt(coords[1]);
-            player = new Player(new Position(x, y));
-
-            line = reader.readLine();
-            int exp = Integer.parseInt(line);
-            player.setExperience(exp);
-
-            line = reader.readLine();
-            List<Player.ArtifactInInventory> artifactInInventories = new ArrayList<>();
-            for (int i = 0; i < line.length(); i++) {
-                char c = line.charAt(i);
-                if (c == ' ') {
-                    continue;
-                }
-                Artifact artifact = GenerationUtils.makeArtifact(c, new Position(-1, -1));
-                artifactInInventories.add(new Player.ArtifactInInventory(artifact));
-            }
-
-            line = reader.readLine();
-            String[] equipFlags = line.split(" ");
-
-            for (int i = 0; i < equipFlags.length; i++) {
-                if (equipFlags[i].equals("true")) {
-                    artifactInInventories.get(i).equip();
-                }
-            }
-
-            player.setArtifacts(artifactInInventories);
-
-            line = reader.readLine();
-            setFeatures(player, line);
-
-            List<AbstractGameObject> lineList = field.get(x);
-            lineList.set(y, player);
-            field.set(x, lineList);
-
-            // GETTING KEY INFO
-            line = reader.readLine();
-            coords = line.split("\\s+");
-            x = Integer.parseInt(coords[0]);
-            y = Integer.parseInt(coords[1]);
-            key = new FinalKey(new Position(x, y));
-
-            lineList = field.get(x);
-            lineList.set(y, key);
-            field.set(x, lineList);
-
-            // GETTING MOBS INFO
-            line = reader.readLine();
-            int mobsNum = Integer.parseInt(line);
-
-            for (int i = 0; i < mobsNum; i++) {
-                line = reader.readLine();
-                coords = line.split("\\s+");
-                x = Integer.parseInt(coords[0]);
-                y = Integer.parseInt(coords[1]);
-
-                char c = mapFromFile.get(x).charAt(y);
-                Mob mob = GenerationUtils.makeMob(c, new Position(x, y));
-
-                line = reader.readLine();
-                setFeatures(mob, line);
-
-                mobs.add(mob);
-
-                lineList = field.get(x);
-                lineList.set(y, mob);
-                field.set(x, lineList);
-            }
-        } catch (IOException e) {
-            RoguelikeLogger.INSTANCE.log_error("I/O error during loading game");
-        }
-
-        return new GameModelImpl(field, player, key, mobs, artifacts);
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+        ByteArrayInputStream bis = new ByteArrayInputStream(fileContent);
+        ObjectInput in = new ObjectInputStream(bis);
+        Object o = in.readObject();
+        return (GameModel)o;
     }
 
     /**
@@ -242,49 +93,6 @@ public class GameSaverAndLoader {
     public static void deleteGame() {
         File file = new File(DEFAULT_SAVED_GAME_FILENAME);
         file.delete();
-    }
-
-    private String getFeaturesAsString(AbstractGameParticipant participant) {
-        return participant.getFullHealth() + " " +
-               participant.getHealth() + " " +
-               participant.getPhysicalDamage() + " " +
-               participant.getFireDamage() + " " +
-               participant.getFreezeProbability() + " " +
-               participant.getFireProbability() + " " +
-               participant.getPhysicalDamageMultiplier() + " " +
-               participant.getFireDamageMultiplier() + " " +
-               participant.getRegeneration() + " " +
-               participant.getFreezeCount() + " " +
-               participant.getFireCount() + " " +
-               participant.getFireValue();
-    }
-
-    private void setFeatures(AbstractGameParticipant participant, String featuresString) {
-        String[] features = featuresString.split("\\s+");
-        participant.setFullHealth(Integer.parseInt(features[0]));
-        participant.setHealth(Integer.parseInt(features[1]));
-        participant.setPhysicalDamage(Integer.parseInt(features[2]));
-        participant.setFireDamage(Integer.parseInt(features[3]));
-        participant.setFreezeProbability(Double.parseDouble(features[4]));
-        participant.setFireProbability(Double.parseDouble(features[5]));
-        participant.setPhysicalDamageMultiplier(Double.parseDouble(features[6]));
-        participant.setFireDamageMultiplier(Double.parseDouble(features[7]));
-        participant.setRegeneration(Integer.parseInt(features[8]));
-        participant.setFreezeCount(Integer.parseInt(features[9]));
-        participant.setFireCount(Integer.parseInt(features[10]));
-        participant.setFireValue(Integer.parseInt(features[11]));
-    }
-
-    private static void appendToFile(String filePath, String text) {
-        File file = new File(filePath);
-
-        try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter bw = new BufferedWriter(fw);
-             PrintWriter pw = new PrintWriter(bw)) {
-            pw.print(text);
-        } catch (IOException e) {
-            RoguelikeLogger.INSTANCE.log_error("I/O error during saving game");
-        }
     }
 
     private static void clearFile(String path) throws FileNotFoundException {
--- roguelike/src/test/java/ru/roguelike/logic/gameloading/GameSaverAndLoaderTest.java
+++ roguelike/src/test/java/ru/roguelike/logic/gameloading/GameSaverAndLoaderTest.java
@@ -18,11 +18,11 @@ import static org.junit.Assert.*;
  */
 public class GameSaverAndLoaderTest {
     /**
-     * Testing game save
+     * Testing game save/load
      * @throws IOException
      */
     @Test
-    public void testSavingGame() throws IOException {
+    public void test() throws IOException, ClassNotFoundException {
         String loadGameFilename = "src/maps/map1.txt";
         String savedGameFilename = "test_saving_game.txt";
 
@@ -35,64 +35,14 @@ public class GameSaverAndLoaderTest {
         GameSaverAndLoader saverAndLoader = new GameSaverAndLoader(savedGameFilename);
         saverAndLoader.saveGame(gameModel);
 
-        String actualContent = Utils.readFileAsString(savedGameFilename);
-        String expectedContent = "6 9\n" +
-                "...#M....\n" +
-                "......#..\n" +
-                ".#.P..Y..\n" +
-                ".......c.\n" +
-                ".###.....\n" +
-                ".s..V...k\n" +
-                "2 3\n" +
-                "0\n" +
-                "\n" +
-                "\n" +
-                "100 100 20 5 0.0 0.0 1.0 1.0 5 0 0 0\n" +
-                "5 8\n" +
-                "3\n" +
-                "0 4\n" +
-                "20 20 40 5 0.0 1.0 1.0 1.0 15 0 0 0\n" +
-                "2 6\n" +
-                "50 50 15 0 0.4 0.0 1.0 1.0 10 0 0 0\n" +
-                "5 4\n" +
-                "40 40 10 0 0.0 0.0 1.0 1.0 10 0 0 0\n";
-
-        assertEquals(expectedContent, actualContent);
+        GameModel loaded = saverAndLoader.loadGame();
+        assertEquals(gameModel.getKey().getPosition(), loaded.getKey().getPosition());
+        assertEquals(gameModel.getActivePlayerId(), loaded.getActivePlayerId());
+        assertEquals(gameModel.getMobs().size(), loaded.getMobs().size());
+        assertEquals(gameModel.getArtifacts().size(), loaded.getArtifacts().size());
+        assertEquals(gameModel.getLog(gameModel.getActivePlayerId()), loaded.getLog(loaded.getActivePlayerId()));
 
         savedGameFile.delete();
     }
 
-    /**
-     * Testing game load
-     * @throws IOException
-     */
-    @Test
-    public void testLoadingGame() throws IOException {
-        String loadGameFilename = "src/maps/map1.txt";
-        String savedGameFilename = "test_saving_game.txt";
-
-        File savedGameFile = new File(savedGameFilename);
-        savedGameFile.createNewFile();
-
-        FromFileGenerator generator = new FromFileGenerator(loadGameFilename);
-        GameModel gameModel = generator.generate();
-
-        GameSaverAndLoader saverAndLoader = new GameSaverAndLoader(savedGameFilename);
-        saverAndLoader.saveGame(gameModel);
-
-        GameModel loadedGameModel = saverAndLoader.loadGame();
-
-        assertEquals(gameModel.getField().size(), loadedGameModel.getField().size());
-
-        for (int i = 0; i < gameModel.getField().size(); i++) {
-            assertEquals(gameModel.getField().get(i).size(), loadedGameModel.getField().get(i).size());
-
-            for (int j = 0; j < gameModel.getField().get(i).size(); j++) {
-                assertEquals(gameModel.getField().get(i).get(j).getDrawingFigure(),
-                        loadedGameModel.getField().get(i).get(j).getDrawingFigure());
-            }
-        }
-        
-        savedGameFile.delete();
-    }
 }
